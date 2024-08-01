7eafabe949f20158be8025d98a8aebfe1ebe14f1
Madi Bromfield <madibromfield215@gmail.com>
2024-08-01 15:21:51 -0500

    bare bones...might work...have not tried

--- icecream.js
+++ icecream.js
@@ -1,23 +1,17 @@
-// the object with keys and values
 const order = {
   vanilla: 0,
   chocolate: 0,
   strawberry: 0,
   coffee: 0,
 };
-console.log(order);
-// popup asking for input
-const froyo = prompt(
-  "What Fro-Yo would you like today? Please enter your choices with commas and no spaces."
+const froyoInput = prompt(
+  "Please enter an order in all lowercase, using commas and no spaces. Thank you!"
 );
-
-// instructions that are repeated to look at input and split up the orders at the commas
-function onlinOrder(froyo) {
-  //   varriable that can be returned "splitUp"
-  const splitUp = froyo.split(",");
-  for (i = 0; i < splitUp.length; i++) {
-    order[splitUp[i]] += 1;
-  }
-  console.log(order(flavor));
-  return order;
-}
+function update(froyo) {
+  const flavors = froyo.split(",");
+flavors.forEach(froyo => {
+    if (flavors in order)) {
+        order[flavor] += 1;
+    }
+});
+console.table(order);
