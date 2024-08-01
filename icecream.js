2d0a5d901fff165281b22c5ae2901ebc62207d07
Madi Bromfield <madibromfield215@gmail.com>
2024-08-01 15:34:17 -0500

    i think it works

--- icecream.js
+++ icecream.js
@@ -1,17 +1,31 @@
+// WARNING: The notes in this may not fully be correct. After doing research this is what I came up with along with my understanding of what is happening.
+
+// the keys and values what is known as the object
 const order = {
   vanilla: 0,
   chocolate: 0,
   strawberry: 0,
   coffee: 0,
 };
+// the alert asking for the order create an array from input
 const froyoInput = prompt(
-  "Please enter an order in all lowercase, using commas and no spaces. Thank you!"
+  "Please enter an order in all lowercase, using commas and no spaces. Your options are... vanilla, chocolate, strawberry, or chocolate.  Thank you!"
 );
+// taking the order and making
 function update(froyo) {
+  // takes array from input and splits it at the commas
   const flavors = froyo.split(",");
-flavors.forEach(froyo => {
-    if (flavors in order)) {
-        order[flavor] += 1;
+  // counts each flavor in the array
+  flavors.forEach(function (flavor) {
+    // adds a number to the original object every time the counter finds the similar item
+    if (order.hasOwnProperty(flavor)) {
+      order[flavor] += 1;
     }
-});
-console.table(order);
+  });
+  //   shows the added flavors within a table
+  console.table(order);
+  //   exits function
+  return order;
+}
+// calls on the update function for the prompt customer input
+update(froyoInput);
